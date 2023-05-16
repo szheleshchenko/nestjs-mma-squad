2c76cab6f7951d101cda985015c6acbe323b39db
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2023-05-17 00:39:13 +0600

    fix(event-scheduler): fixed ufc events urls scraping

--- src/frameworks/web-scrapper/cheerio/cheerio-event-web-scrapper.service.ts
+++ src/frameworks/web-scrapper/cheerio/cheerio-event-web-scrapper.service.ts
@@ -2,44 +2,48 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import * as cheerio from 'cheerio';
 import { lastValueFrom, of, switchMap } from 'rxjs';
+import { appConfig } from 'src/config';
 import { EventWebScrapperService } from 'src/core/abstracts/web-scrapper';
 
 @Injectable()
 export class CheerioEventWebScrapperService extends EventWebScrapperService {
-  constructor(
-    private httpService: HttpService
-  ) {
+  constructor(private httpService: HttpService) {
     super();
   }
 
-  public scrape({ url, eventURLs }: { url: string, eventURLs: Array<string> }): Promise<Array<string>> {
-    const data = this.httpService.get<string>(url)
-      .pipe(
-        switchMap((response) => {
-          const $ = cheerio.load(response.data);
+  public scrape({
+    url,
+    eventURLs = []
+  }: {
+    url: string;
+    eventURLs: Array<string>;
+  }): Promise<Array<string>> {
+    const data = this.httpService.get<string>(url).pipe(
+      switchMap((response) => {
+        const $ = cheerio.load(response.data);
 
-          eventURLs.concat(this.extract($));
+        eventURLs = eventURLs.concat(this.extract($));
 
-          const nextPageLink = this.getNextPageLink($);
+        const nextPageLink = this.getNextPageLink($);
 
-          if (nextPageLink) {
-            return this.scrape({ url, eventURLs });
-          }
+        if (nextPageLink) {
+          return this.scrape({ url: nextPageLink, eventURLs });
+        }
 
-          return of(eventURLs);
-        })
-      )
-    ;
-
+        return of(eventURLs);
+      })
+    );
     return lastValueFrom(data);
   }
-  
+
   public extract($: cheerio.CheerioAPI): Array<string> {
     const eventURLs = [];
 
     $('tr[itemtype="http://schema.org/Event"] a[itemprop="url"]').each(
       (_, element) => {
         const eventURL = $(element).attr('href');
+
+        console.log(eventURL);
 
         eventURLs.push(eventURL);
       }
@@ -48,7 +52,21 @@ export class CheerioEventWebScrapperService extends EventWebScrapperService {
     return eventURLs;
   }
 
-  public getNextPageLink($: cheerio.CheerioAPI): string {
-    return $('a[title="Older Events »"]').attr('href');
+  public getNextPageLink($: cheerio.CheerioAPI): string | null {
+    let url = '';
+
+    $('span.pagination a').each((_, element) => {
+      const formattedElement = $(element);
+
+      if (formattedElement.text().includes('Older Events')) {
+        url = formattedElement.attr('href');
+      }
+    });
+
+    if (!url) {
+      return null;
+    }
+
+    return appConfig.sherdog.url + url;
   }
 }
